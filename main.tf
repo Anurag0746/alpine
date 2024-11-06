42c71ea6af3e3aab3c69a6913ad8b9809a818d71
Anurag Kumar <you@example.com>
2024-11-06 07:32:15 +0530

    add new code

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "hcl"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "jio"
+  location = "centralindia"
+}
